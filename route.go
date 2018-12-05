64d6ec5e614f7930e9ce64a017c7fc355ed587eb
thosapol <isocare.thospol@gmail.com>
2018-12-05 23:32:48 +0700

    fixed: route KpayMerchant

--- route.go
+++ route.go
@@ -9,18 +9,20 @@ import (
 func initializeRoutes() *gin.Engine {
 
 	r := gin.Default()
+	register := r.Group("/register")
+	register.POST("/", CreateMerchantEndPoint)
+
 	merchant := r.Group("/merchant")
 	merchant.Use(middleware.BasicAuthenMerchant)
 	merchant.GET("/", AllMerchantEndPoint)
-	merchant.POST("/register", CreateMerchantEndPoint)
-	merchant.GET("/register/:id", FindByIdMerchantEndPoint)
-	merchant.POST("/register/:id", UpdateIdMerchantEndPoint)
+	merchant.GET("/:id", FindByIdMerchantEndPoint)
+	merchant.POST("/:id", UpdateIdMerchantEndPoint)
 
-	merchant.GET("/register/:id/products", FindByIdMerchantEndPoint)
-	merchant.POST("/register/:id/product", CreateProductMerchantEndPoint)
-	merchant.DELETE("/register/:id/product/:product_id", DeleteProductMerchantEndPoint)
-	merchant.POST("/register/:id/product/:product_id", UpdateProductMerchantEndPoint)
-	merchant.GET("/register/:id/report", FindByIdReportMerchantEndPoint)
+	merchant.GET("/:id/products", FindByIdMerchantEndPoint)
+	merchant.POST("/:id/product", CreateProductMerchantEndPoint)
+	merchant.DELETE("/:id/product/:product_id", DeleteProductMerchantEndPoint)
+	merchant.POST("/:id/product/:product_id", UpdateProductMerchantEndPoint)
+	merchant.GET("/:id/report", FindByIdReportMerchantEndPoint)
 	merchant.GET("/report", AllReportMerchantEndPoint)
 
 	buy := r.Group("/buy")
